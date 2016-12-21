f9a64ac0c11722a28daa824c0f28bf5f82d85f81
George Katsanos <george@gplus.gr>
2016-12-21 16:49:30 +0100

    Attempting Vue-router integration

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # gplus
-Gplus website
 
-My personal websites source code. Not much to see here. Head to www.gplus.gr
+> George Katsanos portfolio website
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# run unit tests
+npm run unit
+
+# run all tests
+npm test
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/Hello/Hello.vue
@@ -0,0 +1,43 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="wrapper text-center">
+        <h1 class="hello">ジョージ・カチャノス</h1>
+        <h2 class="subtitle">Berlin-Based Front-end Developer</h2>
+      </div>
+    </div>
+    <footer>
+      <div class="social list-flex">
+        <li><a href="https://github.com/gkatsanos">GITHUB</a></li>
+        <li><a href="https://medium.com/@gplusgr">MEDIUM</a></li>
+        <li>
+          <a href="http://stackoverflow.com/users/458060/george-katsanos">STACKOVERFLOW</a>
+        </li>
+        <li><a href="https://www.codeschool.com/users/860136">CODESCHOOL</a></li>
+        <li><a href="http://www.codewars.com/users/gkatsanos">CODEWARS</a></li>
+        <li>
+          <a href="http://de.linkedin.com/in/georgekatsanos">LINKEDIN</a>
+        </li>
+        <li><a href="https://www.hackerrank.com/gkatsanos">HACKERRANK</a></li>
+        <li>
+          <router-link to"/cv">CV</router-link>
+        </li>
+      </div>
+    </section>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
