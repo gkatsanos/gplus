0481bffb407adb460c930d5ed847bf60f50a5ee7
George Katsanos <george@gplus.gr>
2017-03-03 22:39:07 +0100

    Grid fixes and menu height

--- src/components/Navigation/Navigation.vue
+++ src/components/Navigation/Navigation.vue
@@ -1,22 +1,20 @@
 <style src="./navigation.scss" lang="scss" scoped></style>
 
 <template>
-  <div>
-    <ul>
-      <li>
-        <router-link to="/" exact>intro</router-link>
-      </li>
-      <li>
-        <router-link to="/resume">résumé</router-link>
-      </li>
-      <li>
-        <router-link to="/projects">projects</router-link>
-      </li>
-      <li>
-        <a href="https://medium.com/@gplusgr" target="_blank">writings</a>
-      </li>
-    </ul>
-  </div>
+  <ul>
+    <li>
+      <router-link to="/" exact>intro</router-link>
+    </li>
+    <li>
+      <router-link to="/resume">résumé</router-link>
+    </li>
+    <li>
+      <router-link to="/projects">projects</router-link>
+    </li>
+    <li>
+      <a href="https://medium.com/@gplusgr" target="_blank">writings</a>
+    </li>
+  </ul>
 </template>
 
 <script>
