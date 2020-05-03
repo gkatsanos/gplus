1ff4c6a81c9e026fdf6421a420ccd93b80e377be
Georgios Katsanos <georgios.katsanos@here.com>
2020-05-03 12:45:24 +0200

    Updating intro page copy text and fixing animation

--- src/pages/Intro/Intro.vue
+++ src/pages/Intro/Intro.vue
@@ -16,11 +16,11 @@
       </h2>
     </div>
     <div class="skills">
-      SCSS & JS architecture
-      <span class="highlight">//</span>
       Vue, React, Node, Express
       <span class="highlight">//</span>
-      webpack & scaffolding
+      Mobile & Responsive Web
+      <span class="highlight">//</span>
+      SCSS & JS architecture
       <br />
       performance optimization
       <span class="highlight">//</span>
@@ -28,7 +28,7 @@
     </div>
     <div class="social-links flex" role="navigation">
       <a rel="nofollow" href="https://github.com/gkatsanos/" target="_blank">
-        <i class="icon-github"></i>
+        <i class="icon icon-github"></i>
         github
       </a>
       <a
@@ -36,15 +36,15 @@
         href="http://stackoverflow.com/users/458060/george-katsanos"
         target="_blank"
       >
-        <i class="icon-stackoverflow"></i>
+        <i class="icon icon-stackoverflow"></i>
         stackoverflow
       </a>
       <a rel="nofollow" href="https://www.linkedin.com/in/georgekatsanos" target="_blank">
-        <i class="icon-linkedin-square"></i>
+        <i class="icon icon-linkedin-square"></i>
         linkedin
       </a>
       <a rel="nofollow" href="https://medium.com/@gplusgr" target="_blank">
-        <i class="icon-medium"></i>
+        <i class="icon icon-medium"></i>
         medium
       </a>
     </div>
@@ -56,7 +56,7 @@ export default {
   name: "Intro",
   data() {
     return {
-      headline: "HUMAN-CENTERED*DESIGN & TECHNOLOGY",
+      headline: "HUMAN-CENTERED*WEB DEVELOPMENT",
     };
   },
   computed: {
